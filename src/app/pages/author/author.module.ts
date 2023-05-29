d8e2336909458a56caa79b7971a9487cee160714
christianfe <christian.felicione@gmail.com>
2023-05-29 14:32:13 +0200

    small fixes

--- src/app/pages/author/author.module.ts
+++ src/app/pages/author/author.module.ts
@@ -8,16 +8,13 @@ import { AuthorPageRoutingModule } from './author-routing.module';
 
 import { AuthorPage } from './author.page';
 import { OperaTabComponent } from 'src/app/components/opera-tab/opera-tab.component';
-import { RoomPageModule } from '../room/room.module';
-import { AppModule } from 'src/app/app.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    AuthorPageRoutingModule,
-    AppModule
+    AuthorPageRoutingModule
   ],
   declarations: [AuthorPage, OperaTabComponent]
 })
