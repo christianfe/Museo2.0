f9073708fc5c767a21a75ca800573f39c94a4d46
Alessio Erasmo <alessio.erasmo001@gmail.com>
2023-05-29 18:48:02 +0200

    started opera page

--- src/app/pages/opera/opera.page.ts
+++ src/app/pages/opera/opera.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Autore } from 'src/app/models/autore';
+import { Opera } from 'src/app/models/opera';
+import { AuthorService } from 'src/app/services/author.service';
+import { OperasService } from 'src/app/services/operas.service';
 
 @Component({
   selector: 'app-opera',
@@ -7,12 +12,22 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./opera.page.scss'],
 })
 export class OperaPage implements OnInit {
-  idOpera: number | undefined
-  constructor(private route: ActivatedRoute) {
+
+  idOpera: number;
+  opera: Observable<Opera> | undefined;
+  autore: Observable<Autore> | undefined;
+
+  constructor(private route: ActivatedRoute,
+              private operaservice: OperasService,
+              private autoreservice: AuthorService) {
     this.idOpera = route.snapshot.params['id']
   }
 
   ngOnInit() {
+    this.opera = this.operaservice.get(this.idOpera);
+    this.opera.subscribe(opera => {
+      this.autore = this.autoreservice.get(opera.idAutore);
+    })
   }
 
 }
