5cd9a40c8518e8f08c5a60d0ac2dfe11fc41ec17
simonearrotini <moneart01@gmail.com>
2023-06-10 02:30:25 +0200

    qrcode scanner

--- src/app/pages/scan-qr/scan-qr.page.ts
+++ src/app/pages/scan-qr/scan-qr.page.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+
+
+  
 
 @Component({
   selector: 'app-scan-qr',
   templateUrl: './scan-qr.page.html',
   styleUrls: ['./scan-qr.page.scss'],
 })
-export class ScanQRPage implements OnInit {
 
-  constructor() { }
+export class ScanQRPage implements OnInit, OnDestroy{
+
+  
+
+  constructor(private router: Router) {}
+  
+  
+  ngOnDestroy() {
+    BarcodeScanner.showBackground();
+    BarcodeScanner.stopScan();
+  }
 
   ngOnInit() {
+    
+   this.askUser();
+
   }
+
+
+
+  
+  
+  askUser = () => {
+    const c = confirm('Vuoi scansionare un QR code?');
+
+    if (c) {
+      this.startScan();
+    }
+  };
+
+  startScan = async () => {
+    await BarcodeScanner.checkPermission({ force: true });
+ 
+    BarcodeScanner.hideBackground();
+    const result = await BarcodeScanner.startScan(); 
+
+  
+    if (result.hasContent) {
+
+      this.router.navigate(['tabs/',result.content])
+    }
+  };
 
 }
